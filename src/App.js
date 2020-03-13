7d5fb0e507a590054e8130ff526ec5a977b1a2f2
John <mitchellperlman@gmail.com>
2020-03-13 08:18:50 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LandingPage from './Containers/LandingPage';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Containers/LandingPage.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+
+import { compose, withProps } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+// import MarkerClusterer from "react-google-maps/lib/components/addons/MarkerClusterer";
+
+import fancyMapStyles from '../MapStyle.json';
+
+import Iframe from 'react-iframe';
+
+import Locations from '../Lists/Locations';
+
+import './LandingPage.css';
+
+console.log("locations", Locations);
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDmGDb8cTZ1DXx9l6UmKjcHuOEmNUU7pGA&",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div className="MapContainerElement"  />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) => 
+  <GoogleMap
+    defaultZoom={10.4}
+    defaultCenter={{ lat: 32.849264, lng: -117.210677 }}
+    defaultOptions={{ styles: fancyMapStyles, gestureHandling: 'none', zoomControl: false }}
+    
+   
+  >  
+    {Locations.map( ( loc ) => {
+      return <Marker onClick={() => props.markerclicked(loc.name, loc.myChart)} key={loc.lat} position={{lat: loc.lat, lng: loc.lng }} />
+    })}
+    
+  </GoogleMap>
+)
+
+
+
+export class LandingPage extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        link: '',
+        name: '',
+      }
+    }
+
+    markerClicked =( name, link) => {
+      console.log("clicked", name, link);
+      this.setState({
+        name,
+        link
+      })
+    }
+    render() {
+        return (
+            <div className="LocationsLandingWrapper">
+                <h1>{this.state.name}</h1>
+                <div className="LocationsFlex">
+                    <Iframe 
+                      url={this.state.link}
+                      width="45%"
+                      height="800px"
+                      className="IframeLocationMyChart"
+                    />
+                    <MyMapComponent markerclicked={this.markerClicked} isMarkerShown/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
