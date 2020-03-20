226a56e3bc52fe4e49539d23559431315301a163
John <mitchellperlman@gmail.com>
2020-03-20 14:06:23 -0700

    new layout

--- src/Containers/LandingPage.js
+++ src/Containers/LandingPage.js
@@ -21,8 +21,8 @@ export class LandingPage extends Component {
   constructor(props){
     super(props);
       this.state = {
-        annualPhysical: false,
-        firstTime: false,
+        annualPhysical: true,
+        firstTime: true,
         renderedComponent: 'visitType',
         location: null,
         pcp: null,
@@ -81,19 +81,19 @@ export class LandingPage extends Component {
     }
 
     dynamicVisitType() {
-      //annual exam
+      //annual exam working
       if (this.state.firstTime && this.state.annualPhysical){
         return '534055'
       } 
-      //office visit
+      //office visit working
       if (this.state.firstTime && !this.state.annualPhysical){
         return '534041'
       }
-      // new annual
+      // new annual nothing
       if (!this.state.firstTime && this.state.annualPhysical){
         return '534047'
       }
-      // new patient
+      // new patient nothing
       if (!this.state.firstTime && !this.state.annualPhysical){
         return '534029'
       }
@@ -125,9 +125,9 @@ export class LandingPage extends Component {
       //http://localhost:3000/pcpID/262694 Das Gourab
       //http://localhost:3000/locationID/54206023 Carlsbad
 
-      // const dynamicVisitType = `&vt=${this.dynamicVisitType()}`
+      const dynamicVisitType = `&vt=${this.dynamicVisitType()}`
       const endString = "&view=plain&payor=1001,1003,1012,1013,1014,1017,1020,1023,1024,1029,1032,1039,1042,1050,1058,1072,1086,1088,1091,1093,-1,-2,-3"
-      const url = initialString + providers + locations + defaultVisitType + endString;
+      const url = initialString + providers + locations + dynamicVisitType + endString;
       // https://mychart.perlmanclinic.com/mpc/SignupAndSchedule/EmbeddedSchedule?id=270075,39702,12290,06405,60830,40112,65521,82085,84690,269588,84615,269811,270084,270085,270104,267633,257289,16808,270089,270108,98016,269350,05073,270103,08899,34455,35697,97632,270096,270097,51404,14436,257284,263086,263890,270654,270607,271056,272253,273037,267386,272234,270059,273570,82466,263319,40414,263353,278020,25406,277986,278112,278907,98164,08670,262694,279880,279810,280127&dept=54219023&vt=534412&view=plain&payor=1001,1003,1012,1013,1014,1017,1020,1023,1024,1029,1032,1039,1042,1050,1058,1072,1086,1088,1091,1093,-1,-2,-3
         return (
             <div className="AppointmentsLandingWrapper">
