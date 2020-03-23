bc2714f814c626227129ed3eb83b419d06cc8817
John <mitchellperlman@gmail.com>
2020-03-23 15:10:01 -0700

    iframe trim

--- src/Components/FinalPage/FinalPage.js
+++ src/Components/FinalPage/FinalPage.js
@@ -11,14 +11,19 @@ const FinalPage = (props) => {
     return (
         <div className="FinalPage">
             <BackButton content="Back" changecomponent={() => props.changecomponent('visitType')}/>
-        <h1>{props.location && props.location.name } {pcp && pcp.firstName + ' ' + pcp.lastName + ', ' + pcp.title }  Schedule</h1>
-            <Iframe 
-               url={props.url}
-               height="900px"
-               width="100%"
-               frameBorder="0"
-               className="IframeLocationMyChart"
-           />
+        <h1>{props.location && props.location.name } {pcp && pcp.firstName + ' ' + pcp.lastName + ', ' + pcp.title } Schedule</h1>
+            <div className="ClipIframe">
+                <iframe 
+                   src={props.url}
+                   frameBorder="0"
+                   className="IframeLocationMyChart"
+                   width="720px"
+                   height="525px"
+                   styles={{ clip:"rect(5px, 0px, 0px, 0px)", position: 'absolute', overflow: "hidden"}}
+
+                ></iframe>
+            </div>
+            
         </div>
     );
 }
