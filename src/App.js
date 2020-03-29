f66288d299756a38d7d9813f1945c804eb9bc93a
John <mitchellperlman@gmail.com>
2020-03-29 16:14:51 -0700

    switch router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import LandingPage from './Containers/LandingPage';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-     <Route path='/locationID/:id' render={() =>  <LandingPage/> } />
-     <Route path='/pcpID/:id' render={() =>  <LandingPage/> } />
-     <Route exact path='/' render={() =>  <LandingPage/> } />
+      <Switch>
+        <Route path='/locationID/:id' render={() =>  <LandingPage/> } />
+        <Route path='/pcpID/:id' render={() =>  <LandingPage/> } />
+        <Route path='/' render={() =>  <LandingPage/> } />
+      </Switch>
     </div>
   );
 }
